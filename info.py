885a71d4e6e55af01cda0bf049b310cef8a40547
Pierre Valentin <Focom@users.noreply.github.com>
2017-10-02 19:18:39 -0400

    Translate a few thing, findExecutive still WIP

--- info.py
+++ info.py
@@ -20,7 +20,10 @@ def findAnthem(fileName):
     pattern_good_line = re.compile("(;\">)(.*)(<\/span>)")
     result = pattern_good_line.search(good_line)
     country_file.close()
-    return result.group(2)
+    raw_result = result.group(2)
+    pattern_non_english = re.compile("(\".*\")( )(\()(.*)(\))")
+
+    return 
 
 
 def findLiterracy(fileName):
@@ -56,6 +59,7 @@ def findExport(fileName):
     i = 0
     j = 0
     condition_met = False
+    cond_virgule = False
 
     pattern_all_file = re.compile("<a href=\"../fields/2078.html")
     country_file = open("./factbook/geos/" + fileName, "r", encoding="utf8")
@@ -74,9 +78,29 @@ def findExport(fileName):
                 condition_met = True
     good_line = memory[-9]
     pattern_good_line = re.compile("(\">)(.*)(<\/div)")
-    result = pattern_good_line.search(good_line)
+    raw_result = pattern_good_line.search(good_line).group(2)  # 566 billion (est 2013)
+    pattern_money = re.compile("(\$)(\d*.*)( .*ion)")
+    clean_result = pattern_money.search(raw_result)
+
+
+    sum = clean_result.group(2)
+    float_sum = float(sum)
+    money_adj = clean_result.group(3)
+
+    million = 1000000
+    billion = 1000000000
+    trillion = 1000000000000
+
+    money_adj = pattern_money.search(clean_result.group(0)).group(3)
+    if(money_adj == " million"):
+        result = float_sum * million
+    if(money_adj == " billion"):
+        result = float_sum * billion
+    if(money_adj == " trillion"):
+        result = float_sum * trillion
+
     country_file.close()
-    return result.group(2)
+    return "$" + str(int(result))
 
 
 def findGdpGrowth(fileName):
@@ -104,7 +128,10 @@ def findGdpGrowth(fileName):
     pattern_good_line = re.compile("(\">)(.*)(<\/div)")
     result = pattern_good_line.search(good_line)
     country_file.close()
-    return result.group(2)
+    raw_result = result.group(2)
+    pattern_percent = re.compile("(.*%)")
+    end_result = pattern_percent.search(raw_result)
+    return end_result.group(1)
 
 
 def findGdpPerCapita(fileName):
@@ -130,10 +157,19 @@ def findGdpPerCapita(fileName):
             if test:
                 condition_met = True
     good_line = memory[-17]
-    pattern_good_line = re.compile("(\">)(.*)(<\/div)")
-    result = pattern_good_line.search(good_line)
+    pattern_raw_result = re.compile("(\">)(.*)(<\/div)")
+    pattern_result = re.compile("(\$)(\d*,\d*)")
+    raw_result = pattern_raw_result.search(good_line).group(2)
+    result = pattern_result.search(raw_result).group(2)
     country_file.close()
-    return result.group(2)
+
+    list_of_output = ["$"]
+    for letter in result:
+        if(letter != ","):
+            list_of_output.append(letter)
+
+    result_clean = "".join(list_of_output)
+    return result_clean
 
 
 def findHazard(fileName):
@@ -164,7 +200,6 @@ def findHazard(fileName):
     hazard_pattern2 = re.compile(".[Hh]urricanes.")
     hazard_pattern3 = re.compile(".[Cc]yclone.")
     hazard_pattern4 = re.compile(".[Tt]yphoons.")
-    hazard_pattern5 = re.compile(".[Aa]ir.")
 
     result = False
 
@@ -173,7 +208,6 @@ def findHazard(fileName):
         test2 = hazard_pattern2.search(memory[starting_number])
         test3 = hazard_pattern3.search(memory[starting_number])
         test4 = hazard_pattern4.search(memory[starting_number])
-        test5 = hazard_pattern5.search(memory[starting_number])
 
         starting_number = starting_number + 1
         if(test1):
@@ -188,8 +222,74 @@ def findHazard(fileName):
         if(test4):
             result = True
             break
-        if(test5):
-            result = True
+    country_file.close()
+    if (result):
+        return "YES"
+    else:
+        return "NO"
+
+
+def findExecutive(fileName):
+    memory = []
+    i = 0
+    Title = ""
+    condition_met = False
+
+    pattern_all_file = re.compile("[chief|head] of state:")
+    pattern_span = re.compile("(;\">)(.*)(</span>)")
+    country_file = open("./factbook/geos/" + fileName, "r", encoding="utf8")
+
+    for line in country_file:
+        if (condition_met):
+            memory.insert(i, line)
+            test_span = pattern_span.search(line)
+            if(test_span):
+                raw_result = test_span.group(2)
+                print(test_span.group(2))
+                break
+        else:
+            memory.insert(i, line)
+            i = i + 1
+            test = pattern_all_file.search(line)
+            if test:
+                condition_met = True
+
+    Title1_pattern = re.compile("Queen ")
+    Title2_pattern = re.compile("King ")
+    Title3_pattern = re.compile("Prince ")
+    Title4_pattern = re.compile("President ")
+
+    Title1 = Title1_pattern.search(raw_result)
+    Title2 = Title2_pattern.search(raw_result)
+    Title3 = Title3_pattern.search(raw_result)
+    Title4 = Title4_pattern.search(raw_result)
+
+    k = 2
+    while True:
+
+        if(Title1):
+            Title = "Queen"
             break
-    country_file.close()    
-    return result
+        if(Title2):
+            Title = "King"
+            break
+        if(Title3):
+            Title = "Prince"
+            break
+        if(Title4):
+            Title = "President"
+            break
+        k += 1
+        if (k == 10000000000):
+            Title = "Unknown"
+            break
+    # print(Title)
+        # (President)(.*)(\(since)
+    #print (name_head_of_state.group(2))
+
+    pattern_name = re.compile("(" + Title + ")(.*)(\(since)")
+    name_head_of_state = pattern_name.search(raw_result)
+
+    country_file.close()
+
+    return(name_head_of_state.group(2) + ", " + Title)
